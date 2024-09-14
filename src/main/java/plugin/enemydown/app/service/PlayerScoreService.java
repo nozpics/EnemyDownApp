4b19b4b0397294764b393d72e2df48b2941143e7
nozpics <nozpics0707@gmail.com>
2024-09-14 11:10:03 +0900

    firstCommit

---
+++ src/main/java/plugin/enemydown/app/service/PlayerScoreService.java
@@ -0,0 +1,22 @@
+package plugin.enemydown.app.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import plugin.enemydown.app.mapper.GameConfigMapper;
+import plugin.enemydown.app.mapper.PlayerScoreMapper;
+import plugin.enemydown.app.mapper.data.GameConfig;
+import plugin.enemydown.app.mapper.data.PlayerScore;
+
+@Service
+public class PlayerScoreService {
+
+  public final PlayerScoreMapper mapper;
+
+  public PlayerScoreService(PlayerScoreMapper mapper){
+    this.mapper = mapper;
+  }
+
+  public List<PlayerScore> searchPlayerScoreList(){
+    return mapper.selectPlayerScoreList();
+  }
+}
